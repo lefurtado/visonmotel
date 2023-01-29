ae96c9596f9b032b4ae2f0de090bec5347f03fd2
Leandro Furtado <le_furtado@outlook.com.br>
2023-01-29 19:03:44 -0300

    responsivo

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,10 +4,13 @@ const navTop = nav.offsetTop;
 window.onscroll = function () {
     fixOnTop();
 }
+
 function fixOnTop() {
     if (window.pageYOffset >= navTop) {
         nav.classList.add("fixOnTop");
+        nav.classList.add("background__nav");
     } else {
         nav.classList.remove("fixOnTop");
+        nav.classList.remove("background__nav");
     }
 }
